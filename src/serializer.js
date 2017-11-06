a85343e4d26fa0a4a2b3619bef9f9d8abd5a8bf2
heveritt <hugo.everitt@gmail.com>
2017-11-06 16:43:36 +0000

    Re-factor serializer to use closures for the reviver and replacer functions

--- src/serializer.js
+++ src/serializer.js
@@ -2,54 +2,57 @@ class Serializer {
 
   constructor(classMap) {
     this.classMap = classMap;
-    this.alreadySerealized = undefined;
-
-    this.replacer = this.storeClass.bind(this);
-    this.reviver = this.restoreClass.bind(this);
   }
 
   serialize(instance) {
-    this.alreadySerialized = new Set();
-    return JSON.stringify(instance, this.replacer);
+    return JSON.stringify(instance, this.replacer());
   }
 
   deserialize(jsonString) {
-    return JSON.parse(jsonString, this.reviver);
+    return JSON.parse(jsonString, this.reviver());
   }
 
-  storeClass(key, value) {
+  replacer() {
+    const classMap = this.classMap;
+    const alreadySerialized = new WeakSet();
 
-    if (typeof value === 'object') {
-      const className = value.constructor.name;
+    return function storeClass(key, value) {
+      if (typeof value === 'object') {
+        const className = value.constructor.name;
 
-      if (this.alreadySerialized.has(value)) {
-        throw new Error('Circular dependency detected - class: ' + className);
-      } 
-      this.alreadySerialized.add(value);
+        if (alreadySerialized.has(value)) {
+          throw new Error('Circular dependency detected - class: ' + className);
+        } 
+        alreadySerialized.add(value);
 
-      if (this.classMap[className]) {
-        // Need to shallow copy object to avoid infinite recursion
-        return ['class: ' + className, Object.assign({}, value)];
+        if (classMap[className]) {
+          // Need to shallow copy object to avoid infinite recursion
+          return ['class: ' + className, Object.assign({}, value)];
+        }
       }
-    }
 
-    return value;
+      return value;
+    }
   }
 
-  restoreClass(key, value) {
-    if (Array.isArray(value) && 
-        typeof value[0] === 'string' && 
-        value[0].slice(0, 7) === 'class: ') {
-      const className = value[0].slice(7);
-      const properties = value[1];
-      const constructor = this.classMap[className];
-      if (constructor) {
-        return new constructor(properties);
-      } else {
-        throw new Error('Atttempt to de-serialize unmapped class: ' + className);
+  reviver() {
+    const classMap = this.classMap;
+
+    return function restoreClass(key, value) {
+      if (Array.isArray(value) && 
+          typeof value[0] === 'string' && 
+          value[0].slice(0, 7) === 'class: ') {
+        const className = value[0].slice(7);
+        const properties = value[1];
+        const constructor = classMap[className];
+        if (constructor) {
+          return new constructor(properties);
+        } else {
+          throw new Error('Atttempt to de-serialize unmapped class: ' + className);
+        }
       }
+      return value;
     }
-    return value;
   }
 
 }

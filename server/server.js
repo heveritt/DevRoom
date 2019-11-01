6330e9af444a0e8a5c2d6422aaddbead9eb9d962
heveritt <hugo.everitt@gmail.com>
2019-11-01 11:20:53 +0000

    Small server changes before parking server for a period

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 
 const server = express();
+const port = 3001;
+
 server.use(express.static('build'));
-server.listen(3001, function() {
-    console.log('Server started on port 3001');
+
+server.get('nodes/:nodeId', (req, res) => {
+    console.log('Requested node: ' + req.params.nodeId);
+});
+
+server.use( (req, res, next) => {
+    res.status(404).send("404 - Page not found!");
+});
+
+server.use( (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("500 - Application Error!");
+});
+
+server.listen(port, () => {
+    console.log('Server started on port '  + port);
 });
